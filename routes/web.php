4fc388d5df824d4595907a1650d96b9cfd3787b3
raihan <raihanulmcse@mail.com>
2023-04-02 14:08:30 +0800

    login page created

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,17 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/home',[StudentsController::class,"index"]);
+Route::get('/',[StudentsController::class,"index"]);
+Route::get('/login',[LoginController::class,"index"]);
+Route::get('/register',[RegisterController::class,"index"]);
+Route::get('/',[StudentsController::class,"index"]);
 Route::get("/create",[StudentsController::class, 'create'])->name('create');
 Route::post("/store",[StudentsController::class, 'store'])->name('store');
 Route::get("/edit/{studentId}",[StudentsController::class, 'update'])->name('edit');
@@ -30,3 +27,6 @@ Route::get('blog/details/{studentId}', [BlogController::class, 'show'])->name('b
 
 Route::post("/blog/store",[BlogController::class, 'store'])->name('blog.store');
 Route::resource('blog', BlogController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
